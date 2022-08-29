76be61b4d926512a8edccab8a6933a55cc04adb8
QiPeng <pq771271668@users.noreply.github.com>
2022-08-29 10:33:22 +0800

    Add files via upload

--- prompts.js
+++ prompts.js
@@ -1,6 +1,6 @@
 module.exports = [{
-	name: 'name', // 这个问题得到答案后，答案储存给那个字段，就是你要用的变量名称，key
-	type: 'input', // 问题类型 单选 多选 输入。。。。。
-	message: '请输入项目名（中文）', // 问题内容 给用户显示的问题
-	default: 'ty-school' // 默认值
+	name: 'name',
+	type: 'input',
+	message: '请输入项目名（中文）',
+	default: 'ty-school'
 }]
