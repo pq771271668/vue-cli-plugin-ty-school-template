a8a2ef7b7927a658c610ead6f01851cdf123c209
pq771271668 <771271668@qq.com>
2022-12-05 14:35:41 +0800

    根据项目环境和setting.baseURL值区分baseURL

--- template/src/api/getBaseURL.js
+++ template/src/api/getBaseURL.js
@@ -14,24 +14,31 @@ xhr({
 	url:'./config.json',
 	success:function (resp) {
 		if (resp.length > 0) {
-			/* 走config.json配置网关 */
-			CONFIG.data = resp
-			for (let key of resp) {
-				if ( key.code ==  UrlParams.platformCode) {
-					CONFIG.baseURL = key.url
+			
+			if (process.env.NODE_ENV == 'development') {
+				CONFIG.baseURL = setting.defaultBaseURL
+			}
+			else {
+				/* 走config.json配置网关 */
+				CONFIG.data = resp
+				for (let key of resp) {
+					if ( key.code ==  UrlParams.platformCode) {
+						CONFIG.baseURL = key.url
+					}
+				}
+				/* 如果没有匹配到平台码,则得到一个默认的地址 */
+				/* 默认地址首先取json文件默认地址(code为空),如果没有在取内置的默认地址 */
+				if (!CONFIG.baseURL) {
+					let defaultUrl = resp.find(res => res.code == '')
+					if (defaultUrl) {
+						CONFIG.baseURL = defaultUrl.url
+					}
+					else {
+						CONFIG.baseURL = setting.defaultBaseURL
+					}
 				}
 			}
-			/* 如果没有匹配到平台码,则得到一个默认的地址 */
-			/* 默认地址首先取json文件默认地址(code为空),如果没有在取内置的默认地址 */
-			if (!CONFIG.baseURL) {
-				let defaultUrl = resp.find(res => res.code == '')
-				if (defaultUrl) {
-					CONFIG.baseURL = defaultUrl.url
-				}
-				else {
-					CONFIG.baseURL = setting.defaultBaseURL
-				}
-			}
+			
 		}
 		else {
 			/* 全国一套 */
