41e0470be8175fc8a13b6c3141dadc36c4656bb3
pq771271668 <771271668@qq.com>
2022-12-05 15:31:54 +0800

    优化Mobile/index等

--- template/src/components/common/excel-import/index.vue
+++ template/src/components/common/excel-import/index.vue
@@ -13,31 +13,15 @@
 						<el-link type="primary" @click="onDownload">点击下载导入模板</el-link>
 					</el-form-item>
 					<el-form-item label="导入说明：" >
-						<p>1.下载导入模板，批量填写信息 。</p>
-						<p>2.上传填好的信息表。</p>
-						<p>3.导入时间，与你导入的内容量有关，请耐心等待。</p>
-						<p>4.若导入失败，请根据错误原因修改后重新上传。</p>
+						<slot name="message">
+							<p>1.下载导入模板，批量填写信息 。</p>
+							<p>2.上传填好的信息表。</p>
+							<p>3.导入时间，与你导入的内容量有关，请耐心等待。</p>
+							<p>4.若导入失败，请根据错误原因修改后重新上传。</p>
+						</slot>
 					</el-form-item>
 					<el-form-item label="上传文件：" prop="fileList">
-						<el-upload
-							ref="upload"
-							action=""
-							:limit="limit"
-							:file-list="form.fileList"
-							
-							:auto-upload="false"
-							:http-request="httpRequest"
-							
-							:on-exceed="handleExceed"
-							:before-upload="beforeUpload"
-							:on-change="onChange"
-							:on-remove="onRemove"
-							
-							:accept="accept"
-							>
-							<el-button size="medium" type="primary">选择文件</el-button>
-							<div slot="tip" class="el-upload__tip">只能上传{{limit}}个excel文件</div>
-						</el-upload>
+						<upload v-bind="$attrs" v-on="$listeners"></upload>
 					</el-form-item>
 				</el-form>
 			</div>
@@ -50,146 +34,63 @@
 </template>
 
 <script>
-	import Error from  './error.js'
-	import axios from 'axios'
-	export default {
-		name:'import',
-		data () {
-			return {
-				show:false,
-				form:{
-					fileList:[]
-				},
-				rules:{
-					fileList:[
-						{ required: true, message: '请选择上传文件', trigger: 'blur' },
-					]
-				},
-				limit:1,
-				accept:'.xls,.xlsx'
-			}
-		},
-		methods:{
-			onDownload () {
-				axios.get(Object.assign({},this.download,{
-					responseType: "blob"
-				}))
-				.then ( res => {
-					if(!res) return
-					const blob = new Blob([res.data], { type: 'application/vnd.ms-excel' }) // 构造一个blob对象来处理数据，并设置文件类型
-					 
-					const filename = this.name+'模板_'+this.$util.timeFormat(new Date(),'yyyy_MM_dd_hh_mm_ss') 
-					if (window.navigator.msSaveOrOpenBlob) { //兼容IE10
-						navigator.msSaveBlob(blob, filename)
-					} else {
-						const href = URL.createObjectURL(blob) //创建新的URL表示指定的blob对象
-						const a = document.createElement('a') //创建a标签
-						a.style.display = 'none'
-						a.href = href // 指定下载链接
-						a.download = filename //指定下载文件名
-						a.click() //触发下载
-						URL.revokeObjectURL(a.href) //释放URL对象
-					}
-				})
-			},
-			handleExceed(files, fileList) {
-				this.$tyToast(`<p class="line-height-24 font-16">当前限制选择 ${this.limit} 个文件</p><p class="line-height-24 font-16">本次选择了 ${files.length} 个文件</p><p class="line-height-24 font-16">共选择了 ${files.length + fileList.length} 个文件</p>`);
-			},
-			
-			beforeUpload(file) {
-				const xls = file.type === 'application/vnd.ms-excel';
-				const xlsx = file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-		
-				if (!xls && !xlsx) {
-				  this.$tyToast('上传文件只能是Excel格式文件!');
-				}
-				return xls || xlsx;
-			},
-			httpRequest (param) {
-				const formData = new FormData()
-				formData.append('file', param.file)
-				
-				this.$axios[this.upload.axiosName]({
-					data:formData
-				})
-				.then( res => {
-					// console.log(res);
-					
-					this.$axios[this.import.axiosName]({
-						data:Object.assign({},this.import.data,{
-							excelUrl:res
-						})
-					})
-					.then( resp => {
-						// console.log(resp)
-						if (resp.successCount == -1) {
-							let arr = _.compact(resp.message.split('<br>'))
-							
-							let errorData = arr.map( item => {
-								const _item = item.split('，')
-								return {
-									rowIndex:_item[0],
-									msg:_item[1]
-								}
-							})
-							// console.log(errorData)
-							Error({
-								tableData:errorData
-							}) 
-							
-						}
-						else {
-							this.$tyToast({
-								type:'success',
-								message:'导入成功'
-							})
-							this.closed()
-							this.onSuccess && this.onSuccess()
-						}
-						
-					})
-					
-				})
-				
-			},
-			onChange (file, fileList) {
-				this.form.fileList = fileList
-			},
-			onSuccess (response, file, fileList) {
-				this.form.fileList = fileList
-			},
-			onRemove(file, fileList) {
-				this.form.fileList = fileList
-			},
-			cancel () {
-				this.show = false
-				this.visible = false
-			},
-			resetForm(formName) {
-				this.$refs['form'].resetFields();
-			},
-			closed () {
-				this.cancel()
-				this.resetForm()
-			},
-			onImport (success) {
-				this.$refs['form'].validate((valid) => {
-					if (valid) {
-						this.$refs.upload.submit();
-					} else {
-						console.log('error submit!!');
-						return false;
-					}
-				});
-			}
-		},
-		created() {
-			this.show = true
-			this.$nextTick( () => {
-				this.visible = true
-			})
-		}
-	}
+import Error from  './error.js'
+import upload from '@/components/base/upload/index.vue'
+// import axios from 'axios'
+export default {
+    name:'import',
+    components: {
+        upload
+    },
+    data () {
+        return {
+            show:false,
+            form:{
+                fileList:[]
+            },
+            rules:{
+                fileList:[
+                    { required: true, message: '请选择上传文件', trigger: 'blur' },
+                ]
+            },
+            limit:1,
+            accept:'.xls,.xlsx'
+        }
+    },
+    methods:{
+        onDownload () {
+            const filename = this.name+'模板_'+this.$util.timeFormat(new Date(),'yyyy_MM_dd_hh_mm_ss') 
+            this.$util.download('xhr',this.download,filename)
+        },
+        cancel () {
+            this.show = false
+            this.visible = false
+        },
+        resetForm(formName) {
+            this.$refs['form'].resetFields();
+        },
+        closed () {
+            this.cancel()
+            this.resetForm()
+        },
+        onImport (success) {
+            this.$refs['form'].validate((valid) => {
+                if (valid) {
+                    // this.$refs.upload.submit();
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+        }
+    },
+    created() {
+        this.show = true
+        this.$nextTick( () => {
+            this.visible = true
+        })
+    }
+}
 </script>
 
 <style lang="scss" scoped>
--- template/src/views/Mobile/index.vue
+++ template/src/views/Mobile/index.vue
@@ -3,42 +3,17 @@
 </template>
 
 <script>
-	// const VConsole = require('@/assets/js/vconsole');
-	export default {
-		beforeDestroy() {
-			this.$vuex('appWeeklyDetail',{})
-		}
-	}
+export default {
+	
+}
 </script>
 
 <style lang="scss">
-	/* @media screen  and (max-width:1200px){
-		html,body {
-			min-width: auto;
-			// overflow: auto;
-		}
-	} */
 	.app-page {
 		width: 100%;
 		height: 100%;
 		overflow: auto;
 		background-color: $--bg-color-app;
-	}
-	.weekly-page_icon-fixed {
-		position: fixed;
-		// z-index: 1;
-		right: 10px;
-		bottom: 35px;
-		width: 54px;
-		height: 54px;
-		border-radius: 50%;
-		background-color: $--color-primary;
-		// box-shadow: 0 0 10px 0px $--color-primary;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		font-size: 30px;
-		color: #FFFFFF;
 	}
 	.van-button-group {
 		display: flex;
