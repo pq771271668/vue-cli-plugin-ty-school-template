0cd54c2316afb49d299a95ca19779915be16c012
pq771271668 <771271668@qq.com>
2023-06-28 11:22:15 +0800

    getToken优化

--- template/src/api/getToken.js
+++ template/src/api/getToken.js
@@ -10,6 +10,10 @@ import setting from '@/setting'
 
 import { Notify } from 'vant';
 
+import isMobile from '@/assets/js/util/isMobile.js'
+
+import tyToast from '@/components/common/Toast';
+
 function getTicket () {
 	return new Promise( (resolve,reject) => {
 		/* 获取ticket；开发环境通过接口获取，生产环境通过浏览器URL获取 */
@@ -29,7 +33,16 @@ function getTicket () {
 				resolve(res.ticket)
 			})
 			.catch( () => {
-				Notify({ type: 'danger', message: '接口获取ticket失败' });
+				const message = '接口获取ticket失败'
+				if (!isMobile()) {
+					tyToast({
+						type:'error',
+						message
+					})
+				}
+				else {
+					Notify({ type: 'danger', message});
+				}
 			})
 		}
 		else {
@@ -42,33 +55,43 @@ function getTicket () {
 
 function getToken () {
 	return new Promise( (resolve,reject) => {
-		/* token获取地址 */
-		// '/serv-platform/api/usersession/getAccessToken'
-		let url = '/serv-platform/api/usersession/getAccessToken'
-		if (url) {
-			getTicket()
-			.then( (ticket) => {
-				if (!Cookies.get('AccessToken') || _.isEmpty(store.state.USERINFO)) {
-					API.commonToken({
-						params:{
-							platformCode:UrlParams.platformCode,
-							ticket:ticket,
-							appId:UrlParams.appId
-						},
-						token:false,
-						extra:{
-							loading:false
-						}
+		getTicket()
+		.then( (ticket) => {
+			if (!Cookies.get('AccessToken') || _.isEmpty(store.state.USERINFO)) {
+				API.commonToken({
+					params:{
+						platformCode:UrlParams.platformCode,
+						ticket:ticket,
+						appId:UrlParams.appId
+					},
+					token:false,
+					extra:{
+						loading:false
+					}
+				})
+				.then(res => {
+					const accessToken = res.accessToken;
+					
+					Cookies.set("AccessToken", accessToken, {
+						expires: 0.08
 					})
-					.then(res => {
-						const accessToken = res.accessToken;
-						
-						Cookies.set("AccessToken", accessToken, {
-							expires: 0.08
-						})
-						
+					
+					const USERINFO = Object.assign({},res.userSession,{
+						USERROLE:{}
+					})
+					
+					store.commit('$store', {
+						name:'USERINFO',
+						value:USERINFO
+					})
+					
+					if (process.env.NODE_ENV == 'development') {
+						console.log('%c此处可以添加登陆账号的其他信息，例如：用户权限、角色等信息','color:#fff;background-color: #e6a23c;padding: 12px 20px;border-radius: 4px;font-size: 14px;font-weight: 500;');
+					}
+					/* API.commonUserRole().then( resp => {
+						// console.log(resp)
 						const USERINFO = Object.assign({},res.userSession,{
-							USERROLE:{}
+							USERROLE:resp
 						})
 						
 						store.commit('$store', {
@@ -76,39 +99,17 @@ function getToken () {
 							value:USERINFO
 						})
 						
-						if (process.env.NODE_ENV == 'development') {
-							console.log('%c此处可以添加登陆账号的其他信息，例如：用户权限、角色等信息','color:#fff;background-color: #e6a23c;padding: 12px 20px;border-radius: 4px;font-size: 14px;font-weight: 500;');
-						}
-						/* API.commonUserRole().then( resp => {
-							// console.log(resp)
-							const USERINFO = Object.assign({},res.userSession,{
-								USERROLE:resp
-							})
-							
-							store.commit('$store', {
-								name:'USERINFO',
-								value:USERINFO
-							})
-							
-							resolve(USERINFO)
-						}) */
-						
-						
 						resolve(USERINFO)
-					})
-					.catch( () => {
-						Notify({ type: 'danger', message: '接口获取Token和用户信息失败' });
-					})
-				}
-				else {
-					resolve('AccessToken已存在')
-				}
-			})
-		}
-		else {
-			console.error('请设置token和用户信息获取地址')
-			resolve()
-		}
+					}) */
+					
+					
+					resolve(USERINFO)
+				})
+			}
+			else {
+				resolve('AccessToken已存在')
+			}
+		})
 	})
 }
 
