30410704ff7853d700972f8324d5aa19b810b6ba
pq771271668 <771271668@qq.com>
2023-06-29 14:19:24 +0800

    字典项：weekOptions

--- template/src/dict/index.js
+++ template/src/dict/index.js
@@ -1,5 +1,29 @@
 // 字典项
 const dictionary = {
-	
+	/* 星期 */
+	weekOptions:[
+		{
+			label:'星期一',
+			value:1
+		},{
+			label:'星期二',
+			value:2
+		},{
+			label:'星期三',
+			value:3
+		},{
+			label:'星期四',
+			value:4
+		},{
+			label:'星期五',
+			value:5
+		},{
+			label:'星期六',
+			value:6
+		},{
+			label:'星期日',
+			value:7
+		},
+	]
 }
 export default dictionary
